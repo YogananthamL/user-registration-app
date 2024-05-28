c2f1e08be2fb18aee810883cf263c5e0e4e13f5e
Yoganantham L <lsaran825@gmail.com>
2024-05-28 16:28:23 +0530

    Account activation email support

--- src/main/java/com/company/login_app/LoginAppApplication.java
+++ src/main/java/com/company/login_app/LoginAppApplication.java
@@ -2,6 +2,9 @@ package com.company.login_app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 @SpringBootApplication
 public class LoginAppApplication {
@@ -9,5 +12,6 @@ public class LoginAppApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LoginAppApplication.class, args);
 	}
+
 
 }
--- src/main/java/com/company/login_app/services/RegistrationService.java
+++ src/main/java/com/company/login_app/services/RegistrationService.java
@@ -6,6 +6,7 @@ import com.company.login_app.models.Token;
 import com.company.login_app.models.UserRole;
 import com.company.login_app.repositories.TokenRepository;
 import com.company.login_app.repositories.UserRepository;
+import jakarta.mail.MessagingException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -20,7 +21,9 @@ public class RegistrationService {
     private final UserRepository repository;
     private final PasswordEncoder encoder;
     private final TokenRepository tokenRepository;
+    private final EmailService emailService;
 
+    private static final String CONFIRMATION_URL="";
     public String register(UserDTO userDTO){
         //check if the user already exist
         boolean userExists=repository.findByEmail(userDTO.getEmail()).isPresent();
@@ -51,6 +54,12 @@ public class RegistrationService {
                 .user(user)
                 .build();
         tokenRepository.save(token);
+
+        try {
+            emailService.send(userDTO.getEmail(), user.getFirstname(), null, CONFIRMATION_URL);
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
         return generatedToken;
     }
 }
